5bc0cd1163129dec163140ebe39d4586d4654dd5
sinbin1036 <ssb1036@naver.com>
2025-02-12 20:05:16 +0900

    calculator success

--- study/C/calculator.c
+++ study/C/calculator.c
@@ -6,51 +6,49 @@ int main() {
     int result;
     char operand;
 
-    printf("계산할 식을 대입해주세요");
+    printf("계산할 식을 대입해주세요 \n");
     scanf("%d", &num1); //첫번째 숫자 정의
-    printf("%d", &num1);
+    while(getchar() != '\n');
+
+    scanf("%c", &operand);
+    scanf(" %d", &num2); //2번째 숫자 정의
+    while(getchar() != '\n');
+
+    switch (operand)
+{
+        case '+':
+            result = num1 + num2;
+            printf("%d \n", result);
+            break;
+        
+        case '-':
+            result = num1 - num2;
+            printf("%d \n", result);
+            break;  
+
+        case '*':
+            result = num1 * num2;
+            printf("%d \n", result);
+            break;
+        
+        case '/':
+            result = num1 / num2;
+            printf("%d \n", result);
+            break;
+        
+        case '%':
+            result = num1 % num2;
+            printf("%d \n", result);
+            break;
+
+        default:
+            printf("연산자를 잘못 입력하셨습니다. \n");
+            break;
+    }
+
+    printf("아무키나 눌러 종료하세요.");
+    
     getchar();
-
-    // getchar();
-    // scanf("%c", &operand);
-
-    // getchar();
-    // scanf("%d", &num2); //2번째 숫자 정의
-
-    // getchar();
-
-    // switch (operand)
-    // {
-    //     case '+':
-    //         result = num1 + num2;
-    //         printf("%d", &result);
-    //         break;
-        
-    //     case '-':
-    //         result = num1 - num2;
-    //         printf("%d", &result);
-    //         break;  
-
-    //     case '*':
-    //         result = num1 * num2;
-    //         printf("%d", &result);
-    //         break;
-        
-    //     case '/':
-    //         result = num1 / num2;
-    //         printf("%d", &result);
-    //         break;
-        
-    //     case '%':
-    //         result = num1 % num2;
-    //         printf("%d", &result);
-    //         break;
-
-    //     default:
-    //         printf("연산자를 잘못 입력하셨습니다");
-    //         break;
-    // }
-
 
     return 0;
 }
